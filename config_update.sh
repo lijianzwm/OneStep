9d31a07006f9dee1bc9b52b9c37f119fa5d837fe
lijianzwm <lijianzhanwuming@163.com>
2016-06-02 17:15:43 +0800

    添加公共库

--- config_update.sh
+++ config_update.sh
@@ -1,2 +1,2 @@
-scp Application/Common/Conf/config.php root@139.129.22.123:/alidata/www/default/Application/Common/Conf/config.php
-scp Application/Common/Conf/config.php root@139.129.22.123:/alidata/www/OneStep/Application/Common/Conf/config.php
+scp Application/Common/Conf/config.php root@120.27.120.222:/alidata/www/default/Application/Common/Conf/config.php
+#scp Application/Common/Conf/config.php root@139.129.22.123:/alidata/www/OneStep/Application/Common/Conf/config.php
